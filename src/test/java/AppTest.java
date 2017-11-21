48c1366bf888dececbe576399d329a6480a43879
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-21 20:43:26 +0000

    Win 2 puzzle

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,7 +8,8 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class AppTest {
 
@@ -36,40 +37,45 @@ public class AppTest {
         int interactions = 0;
 
         String newLine = "\r";
-        InputStream in = new ByteArrayInputStream(("Adi" + newLine + "y" + newLine + "0" + newLine).getBytes());
+        InputStream in = new ByteArrayInputStream(("Adi" + newLine + "y" + newLine + "1" + newLine).getBytes());
 
         Scanner scanner = new Scanner(in);
 
-        System.out.print("Your name"+ newLine);
+        System.out.print("Your name" + newLine);
         String name = scanner.nextLine();
         interactions++; //one input
         complexity++; //one choice
         System.out.print(name + newLine);
 
-        System.out.print("Are you ready to start? (y/n)"+ newLine);
+        System.out.print("Are you ready to start? (y/n)" + newLine);
         scanner.nextLine();
         interactions++; //one input
         complexity += 2; //number of choices
 
-        System.out.print("| |" + newLine);
+        System.out.print("Place your token to get a line" + newLine);
+        System.out.print("|X| |" + newLine);
         scanner.nextLine();
         interactions++; //one input
-        complexity += 1; //number of choices
+        complexity += 2; //number of choices
 
-        System.out.print("|X|" + newLine);
+        System.out.print("|X|X|" + newLine);
 
-        System.out.print("hello world!" + newLine);
+        System.out.print("You won!" + newLine);
 
         String log = systemOutRule.getLog();
         String[] lines = log.split(newLine);
         assertEquals("Your name", lines[0]);
         assertEquals("Adi", lines[1]);
         assertEquals("Are you ready to start? (y/n)", lines[2]);
-        assertEquals("| |", lines[3]);
-        assertEquals("|X|", lines[4]);
-        assertEquals("hello world!", lines[5]);
-        assertEquals(1+1+1, interactions);
-        assertEquals(1+2+1, complexity);
+        assertEquals("Place your token to get a line", lines[3]);
+        assertEquals("|X| | (press 0 or 1)", lines[4]);
+        assertEquals("|X|X|", lines[5]);
+        assertEquals("You won!", lines[6]);
+        assertEquals(1 + 1 + 1, interactions);
+        assertEquals(1 + 2 + 2, complexity);
+
+        //TODO add a test for various inputs (token at 0, token at wrong number)
+        //TODO add a test for losing
     }
 
     @Test
